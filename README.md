abedd5ab4c70b5290e1b52ab9b1e212aa008bcfc
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2017-12-09 18:10:01 -0500

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # phc-partners
+Display Google Forms application data on Public Humanities Collaborative web page
+
+## Live example
+http://commons.trincoll.edu/cli/phc
+
+## Requirements
+- Google Form responses must appear in Google Sheets, which must be published. Insert URLs in main.js
+- manually insert "Display" column in "Partners" Google Sheet, and manually insert "y" to approve display
+- manually insert "Title" column in "Partners" Google Sheet, and manually insert text to display title
+- embed iframes for Google Form and also for this GH-published repo in the web pages
+
+## Credits
+Created by Ilya Ilyankou (@ilyankou) for Liberal Arts Action Lab, Hartford CT
+
+## Dependencies
+* Tabletop v1.5.2, https://github.com/jsoma/tabletop
+* jQuery v3.2.1, https://jquery.com/
---
+++ main.js
@@ -0,0 +1,112 @@
+var facultyURL = 'https://docs.google.com/spreadsheets/d/1FOYpxQQQ9EZUsuBDR_LKwVsQRz7pHF414g_X27h7ajo/pubhtml';
+
+Tabletop.init({
+  key: facultyURL,
+  callback: processData,
+  simpleSheet: true,
+});
+
+function mergeOrganizationAndWebsite(org, website) {
+  if (!website) { return org; }
+  return '<span class="invisible">' + org + '</span>'
+    + '<a href="http://' + website + '">' + org + '</a>';
+}
+
+function mergeTitleProject(title, project) {
+  res = '<p><span class="emphasis">Title: </span>' + title + '</p>';
+
+  if (project) {
+    res += '<p><span class="emphasis">Public Humanities Project: </span>' + project + '</p>';
+  }
+
+  return res;
+}
+
+
+function emailToLink(name, email) {
+  if (!email) { return name; }
+  return name + ' <a href="mailto:' + email + '"> <i class="fa fa-envelope"> </a>';
+}
+
+
+function processData(data, tabletop) {
+  if (!data[0]) return;
+
+  var processedData = [];
+
+  for (i in data) {
+    var r = data[i];
+    if (r.Display !== 'y') continue;
+
+    // Add a row to the final dataset
+    processedData.push([
+      mergeOrganizationAndWebsite(r.Organization, r.Website) + '<br><br>' + emailToLink(r.Name, r.Email),
+      mergeTitleProject(r.Title, r.Project),
+    ]);
+  }
+
+  // Adding custom filtering
+  /*
+  $.fn.dataTable.ext.search.push(
+    function(settings, data, dataIndex) {
+
+      // This is a JavaScript object whose keys will
+      // be the checked values (e.g. Arts, "Social Sciences")
+      showOnly = {};
+
+      $('input:checkbox:checked').each(function() {
+        showOnly[this.value] = 1;
+      });
+
+      var divisions = data[2].split(',').map(function(x) {return x.trim()});
+
+      for (i in divisions) {
+        if (showOnly[divisions[i]] === 1) {
+          return true;
+        }
+      }
+      return false;
+    }
+  ); */
+
+
+  $(document).ready(function() {
+    var table = $('#results').DataTable({
+      paging: false,
+      info: false,
+      ordering: true,
+      data: processedData,
+      columns: [
+        {title: 'Organization', width: '200px', className: 'td-center'},
+        {title: 'Project', orderable: false},
+      ]
+    });
+
+    $('input[name="filter"]').change(function() {
+      table.draw();
+    });
+
+  });
+
+  /*
+  var filters = [
+    ['Arts', 'Art'],
+    ['Humanities', 'Hum'],
+    ['Sciences', 'Sci'],
+    ['Social Sciences', 'Soc'],
+    ['Public Humanities Collaborative', 'PHC']
+  ] */
+
+  /*
+  function renameCheckboxes() {
+    var shorten = $(window).width() < 900 ? 1 : 0;
+    $('#filters label').each(function(i) {
+      $(this).text(filters[i][shorten])
+    });
+  } */
+
+  /*
+  $(window).resize(renameCheckboxes);
+  renameCheckboxes(); */
+
+}
